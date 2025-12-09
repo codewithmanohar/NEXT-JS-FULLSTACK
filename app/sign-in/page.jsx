1a441252f46195b2325be58b7d10fa9f587c9ca9
codewithmanohar <mksahni2555@gmail.com>
2025-12-09 12:24:41 +0530

    commit : added SignIn using stack-auth

---
+++ app/sign-in/page.jsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@stackframe/stack";
+
+
+export default function SignInPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div>
+                <SignIn />
+            </div>
+        </div>
+    )
+}
