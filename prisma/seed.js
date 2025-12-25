1fdab75d9c1fb12d8bfc2f53f1559b9916a5a0a9
codewithmanohar <mksahni2555@gmail.com>
2025-12-25 13:16:27 +0530

    commit : updated setup and created sidebar for navigation

---
+++ prisma/seed.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const demoUserId = "13544da6-9212-4120-9f9a-ccf8c76857ec";
+
+    await prisma.product.createMany({
+        data: Array.from({ length: 25 }).map((_, i) => ({
+            userId: demoUserId,
+            name: `Product ${i + 1}`,
+            price: Number((Math.random() * 90 + 10).toFixed(2)),
+            quantity: Math.floor(Math.random() * 20),
+            lowStockAt: 5,
+            createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+        }))
+    });
+
+    console.log("Seed data created successfully!");
+    console.log(`Created 25 products for user ID: ${demoUserId}`);
+}
+
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    })
